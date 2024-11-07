183ef982d76f6abee51bceef0349172d88e1aadd
Aryan-Sameer <sameeraryan2005@gmail.com>
2024-11-07 11:05:30 +0530

    added sound

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
+import Spline from '@splinetool/react-spline';
+import { useEffect } from 'react';
 
 function App() {
 
+  useEffect(() => {
+    const playAudio = () => {
+      const audio = new Audio("/spiderman-meme-song.mp3");
+      audio.play().catch(error => {
+        console.log("Autoplay blocked, will wait for user interaction.");
+      });
+    };
+
+    playAudio();
+
+    const handleUserInteraction = () => {
+      playAudio();
+      document.removeEventListener('click', handleUserInteraction);
+    };
+
+    document.addEventListener('click', handleUserInteraction);
+
+    return () => document.removeEventListener('click', handleUserInteraction);
+  }, []);
+
   return (
-    <div>
-      
-    </div>
+    <main className='main'>
+       <Spline scene="https://prod.spline.design/FpuWYfiE2UJxfxS4/scene.splinecode" />
+    </main>
   )
 }
 
