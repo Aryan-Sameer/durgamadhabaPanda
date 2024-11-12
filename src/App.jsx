84b19c6c352948a72a6bd0f5def569c3feacd7ee
Aryan-Sameer <138378287+Aryan-Sameer@users.noreply.github.com>
2024-11-12 16:21:42 +0530

    Update App.jsx
    123587

--- src/App.jsx
+++ src/App.jsx
@@ -54,7 +54,7 @@ function App() {
               />
               <label htmlFor="floatingPasswordCustom">Password</label>
             </Form.Floating>
-            <Button onClick={music} disabled={password == "123578" ? false : true} variant="secondary">Open</Button>
+            <Button onClick={music} disabled={password == "123587" ? false : true} variant="secondary">Open</Button>
           </Card.Body>
         </Card>
       </div > : ""}
